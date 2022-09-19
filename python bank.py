54e4500fe576ed964d16eadf320f1d93e0ccf708
thrqmu <113296352+thrqmu@users.noreply.github.com>
2022-09-19 13:48:40 +0700

    Add files via upload

---
+++ python bank.py
@@ -0,0 +1,12 @@
+# GREETINGS PROGRAM
+
+greetings = str(input("Type your greetings = ")).strip().lower()
+
+if greetings=="hello" :
+    print("$0")
+elif greetings=="hello, newman" :
+    print("$0")
+elif greetings=="how you doing?" :
+    print("$20")
+elif greetings=="what's happening?" :
+    print("$100")
